7a302bc438bb8f29db7543aa3131e34f9c8a793b
Hardik Arora <hardikarora483@gmail.com>
2026-03-13 18:31:41 +0000

    fix: pass contributions as promise to use() hook

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -27,7 +27,7 @@ const Home = async () => {
       {chunks}
     </span>
   );
-  const contributions = await getGitHubContributions();
+  const contributions = getGitHubContributions();
 
   return (
     <main className="flex max-w-screen flex-col items-center font-light">
