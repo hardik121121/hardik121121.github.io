09841dd973938e6c2b87db488b584dccacce2d1a
Hardik Arora <hardikarora483@gmail.com>
2026-03-13 18:14:16 +0000

    feat: configure static export and GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+};
 const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
